8f55f14e1e50a48303f458a364a21cbfec35d2ab
xiaozhangup <xiaozhangshiw@163.com>
2026-09-15 11:09:04 +0800

    Include AuraSkills API on core compile classpath

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
         exclude("dev.dejvokep", "boosted-yaml")
     }
     implementation(project(":compatibility"))
+    compileOnly(files("${rootDir}/compatibility/libs/AuraSkills-2.3.10.jar"))
     // adventure
     implementation("net.kyori:adventure-api:${rootProject.properties["adventure_bundle_version"]}")
     implementation("net.kyori:adventure-text-minimessage:${rootProject.properties["adventure_bundle_version"]}")
